a68a6c9bfef0f8092d17efd5264cc2e2bff6cd9f
Lanota81 <1926974795@qq.com>
2024-08-16 16:14:11 +0800

    fitRustc1.82.0-nightly

---
+++ os/target/debug/deps/semver_parser-e6d594fc242bf9f9.d
@@ -0,0 +1,11 @@
+/root/learn-rCore/os/target/debug/deps/libsemver_parser-e6d594fc242bf9f9.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/version.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/range.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/common.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/recognize.rs
+
+/root/learn-rCore/os/target/debug/deps/libsemver_parser-e6d594fc242bf9f9.rlib: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/version.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/range.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/common.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/recognize.rs
+
+/root/learn-rCore/os/target/debug/deps/semver_parser-e6d594fc242bf9f9.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/version.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/range.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/common.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/recognize.rs
+
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/version.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/range.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/common.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/semver-parser-0.7.0/src/recognize.rs:
--- os/target/riscv64gc-unknown-none-elf/release/deps/os-7f8e3a8dc80a5196.d
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-7f8e3a8dc80a5196.d
@@ -1,8 +1,9 @@
-/root/rCore-Tutorial-v3/os/target/riscv64gc-unknown-none-elf/release/deps/os-7f8e3a8dc80a5196: src/main.rs src/console.rs src/config.rs src/lang_items.rs src/loader.rs src/sbi.rs src/sync/mod.rs src/sync/up.rs src/syscall/mod.rs src/syscall/fs.rs src/syscall/process.rs src/task/mod.rs src/task/context.rs src/task/switch.rs src/task/task.rs src/trap/mod.rs src/trap/context.rs src/task/switch.S src/trap/trap.S src/entry.asm src/link_app.S
+/root/learn-rCore/os/target/riscv64gc-unknown-none-elf/release/deps/os-7f8e3a8dc80a5196: src/main.rs src/boards/qemu.rs src/console.rs src/config.rs src/lang_items.rs src/loader.rs src/sbi.rs src/sync/mod.rs src/sync/up.rs src/syscall/mod.rs src/syscall/fs.rs src/syscall/process.rs src/task/mod.rs src/task/context.rs src/task/switch.rs src/task/task.rs src/timer.rs src/trap/mod.rs src/trap/context.rs src/task/switch.S src/trap/trap.S src/entry.asm src/link_app.S
 
-/root/rCore-Tutorial-v3/os/target/riscv64gc-unknown-none-elf/release/deps/os-7f8e3a8dc80a5196.d: src/main.rs src/console.rs src/config.rs src/lang_items.rs src/loader.rs src/sbi.rs src/sync/mod.rs src/sync/up.rs src/syscall/mod.rs src/syscall/fs.rs src/syscall/process.rs src/task/mod.rs src/task/context.rs src/task/switch.rs src/task/task.rs src/trap/mod.rs src/trap/context.rs src/task/switch.S src/trap/trap.S src/entry.asm src/link_app.S
+/root/learn-rCore/os/target/riscv64gc-unknown-none-elf/release/deps/os-7f8e3a8dc80a5196.d: src/main.rs src/boards/qemu.rs src/console.rs src/config.rs src/lang_items.rs src/loader.rs src/sbi.rs src/sync/mod.rs src/sync/up.rs src/syscall/mod.rs src/syscall/fs.rs src/syscall/process.rs src/task/mod.rs src/task/context.rs src/task/switch.rs src/task/task.rs src/timer.rs src/trap/mod.rs src/trap/context.rs src/task/switch.S src/trap/trap.S src/entry.asm src/link_app.S
 
 src/main.rs:
+src/boards/qemu.rs:
 src/console.rs:
 src/config.rs:
 src/lang_items.rs:
@@ -17,6 +18,7 @@ src/task/mod.rs:
 src/task/context.rs:
 src/task/switch.rs:
 src/task/task.rs:
+src/timer.rs:
 src/trap/mod.rs:
 src/trap/context.rs:
 src/task/switch.S:
