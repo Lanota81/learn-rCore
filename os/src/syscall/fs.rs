85948de04ee544196ac53e4184d204cea5a64d1f
Lanota81 <1926974795@qq.com>
2024-08-17 23:03:39 +0800

    SafeSysWriteCorrected.

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -4,7 +4,7 @@ use crate::batch::{APP_BASE_ADDRESS, APP_SIZE_LIMIT, USER_STACK, USER_STACK_SIZE
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
-            if (((buf as usize) >= USER_STACK.get_sp() - USER_STACK_SIZE) && ((buf as usize) + len <= USER_STACK.get_sp()))
+            if (((buf as usize) >= USER_STACK.get_sp()) && ((buf as usize) + len <= USER_STACK.get_sp() + USER_STACK_SIZE))
                 || (((buf as usize) >= APP_BASE_ADDRESS) && (buf as usize) + len <= APP_BASE_ADDRESS + APP_SIZE_LIMIT) 
             {
                 let slice = unsafe { core::slice::from_raw_parts(buf, len) };
