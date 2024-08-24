3afabda24d97307a141a2c44826258fb937badb8
Lanota81 <1926974795@qq.com>
2024-08-24 19:39:29 +0800

    DocUpdate

--- os/target/release/build/bare-metal-213bca3e22efc19e/build_script_build-213bca3e22efc19e.d
+++
@@ -1,5 +0,0 @@
-/root/learn-rCore/os/target/release/build/bare-metal-213bca3e22efc19e/build_script_build-213bca3e22efc19e: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bare-metal-0.2.5/build.rs
-
-/root/learn-rCore/os/target/release/build/bare-metal-213bca3e22efc19e/build_script_build-213bca3e22efc19e.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bare-metal-0.2.5/build.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bare-metal-0.2.5/build.rs:
