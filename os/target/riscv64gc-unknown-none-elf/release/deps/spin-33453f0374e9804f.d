2eedcfc6bba51bdaed9562d18fbef29c8e269cb7
Lanota81 <1926974795@qq.com>
2024-08-24 19:43:01 +0800

    make clean

--- os/target/riscv64gc-unknown-none-elf/release/deps/spin-33453f0374e9804f.d
+++
@@ -1,9 +0,0 @@
-/root/rCore-Tutorial-v3/os/target/riscv64gc-unknown-none-elf/release/deps/libspin-33453f0374e9804f.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/once.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/relax.rs
-
-/root/rCore-Tutorial-v3/os/target/riscv64gc-unknown-none-elf/release/deps/libspin-33453f0374e9804f.rlib: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/once.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/relax.rs
-
-/root/rCore-Tutorial-v3/os/target/riscv64gc-unknown-none-elf/release/deps/spin-33453f0374e9804f.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/once.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/relax.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/lib.rs:
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/once.rs:
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/spin-0.9.8/src/relax.rs:
