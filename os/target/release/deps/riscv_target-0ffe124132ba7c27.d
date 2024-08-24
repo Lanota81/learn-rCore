112a7fac4f6cf1720a2bdde9070429ba1e2f6876
Lanota81 <1926974795@qq.com>
2024-08-24 19:41:34 +0800

    make_clean

--- os/target/release/deps/riscv_target-0ffe124132ba7c27.d
+++
@@ -1,7 +0,0 @@
-/root/learn-rCore/os/target/release/deps/libriscv_target-0ffe124132ba7c27.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/riscv-target-0.1.2/src/lib.rs
-
-/root/learn-rCore/os/target/release/deps/libriscv_target-0ffe124132ba7c27.rlib: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/riscv-target-0.1.2/src/lib.rs
-
-/root/learn-rCore/os/target/release/deps/riscv_target-0ffe124132ba7c27.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/riscv-target-0.1.2/src/lib.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/riscv-target-0.1.2/src/lib.rs:
--- os/target/riscv64gc-unknown-none-elf/release/deps/buddy_system_allocator-f915a31ed39a0126.d
+++
@@ -1,9 +0,0 @@
-/root/learn-rCore/os/target/riscv64gc-unknown-none-elf/release/deps/libbuddy_system_allocator-f915a31ed39a0126.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/frame.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/linked_list.rs
-
-/root/learn-rCore/os/target/riscv64gc-unknown-none-elf/release/deps/libbuddy_system_allocator-f915a31ed39a0126.rlib: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/frame.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/linked_list.rs
-
-/root/learn-rCore/os/target/riscv64gc-unknown-none-elf/release/deps/buddy_system_allocator-f915a31ed39a0126.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/frame.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/linked_list.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/lib.rs:
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/frame.rs:
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/buddy_system_allocator-0.6.0/src/linked_list.rs:
