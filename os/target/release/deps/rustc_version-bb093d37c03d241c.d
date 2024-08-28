dd595763baeeb3db006eb7f2c3025548634d6c43
Lanota81 <1926974795@qq.com>
2024-08-28 20:02:30 +0800

    make clean

--- os/target/release/deps/rustc_version-bb093d37c03d241c.d
+++
@@ -1,8 +0,0 @@
-/root/rCore-Tutorial-v3/os/target/release/deps/librustc_version-bb093d37c03d241c.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc_version-0.2.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc_version-0.2.3/src/errors.rs
-
-/root/rCore-Tutorial-v3/os/target/release/deps/librustc_version-bb093d37c03d241c.rlib: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc_version-0.2.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc_version-0.2.3/src/errors.rs
-
-/root/rCore-Tutorial-v3/os/target/release/deps/rustc_version-bb093d37c03d241c.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc_version-0.2.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc_version-0.2.3/src/errors.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc_version-0.2.3/src/lib.rs:
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc_version-0.2.3/src/errors.rs:
