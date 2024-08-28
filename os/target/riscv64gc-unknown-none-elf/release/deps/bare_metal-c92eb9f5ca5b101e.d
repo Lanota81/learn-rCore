3239e0a392083b948986950e72f74c629615cc89
Lanota81 <1926974795@qq.com>
2024-08-28 20:00:32 +0800

    cloneCh7-lab

---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/bare_metal-c92eb9f5ca5b101e.d
@@ -0,0 +1,7 @@
+/root/rCore-Tutorial-v3/os/target/riscv64gc-unknown-none-elf/release/deps/libbare_metal-c92eb9f5ca5b101e.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bare-metal-0.2.5/src/lib.rs
+
+/root/rCore-Tutorial-v3/os/target/riscv64gc-unknown-none-elf/release/deps/libbare_metal-c92eb9f5ca5b101e.rlib: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bare-metal-0.2.5/src/lib.rs
+
+/root/rCore-Tutorial-v3/os/target/riscv64gc-unknown-none-elf/release/deps/bare_metal-c92eb9f5ca5b101e.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bare-metal-0.2.5/src/lib.rs
+
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bare-metal-0.2.5/src/lib.rs:
